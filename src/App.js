cc66f2ad4ec3a14366a889cda3cec144ff6e4df8
minandmin237 <78993150+minandmin237@users.noreply.github.com>
2021-02-20 15:32:33 +0700

    photo

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
 import { useState } from "react";
+import Homescreen from "./screen/home_screen";
+
 
 function App() {
-  const [count,setCount] = useState(1);
+  const [count, setCount] = useState(0);
 
-  const addCount = () => {
-setCount(count+1);
-
-  }
+  const addCount = (type = "PLUS") => {
+    if (type === "PLUS") {
+      setCount(count + 1);
+    } else {
+      setCount(count - 1);
+    }
+  };
   return (
     <div>
+      <Homescreen></Homescreen>
+
+      <div>
       <p>คุณกดปุ่มไปแล้ว{count}ครั้ง</p>
-      <button onClick={addCount}>+1</button>
+      <button onClick={() =>addCount("MINUS")}>-1</button>
+      <button onClick={() =>addCount("PLUS")}>+1</button>
+    </div>
     </div>
   );
 }
